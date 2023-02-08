1634ce2e6d8c3930f70e90b3ee7c3e2c399ed7a4
yazdanzv <yazdanzv.1378@gmail.com>
2023-02-08 21:59:19 +0330

    BFS algorithm added

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 from Node import Node
 from Expandation_AStar import Expand_AStar
 from Expandation_UCS import Expand_UCS
+from Expandation_BFS import Expand_BFS
 
 test_node1 = Node(['1', '2', '0', '4', '5', '3', '7', '8', '6'], None, None)
 test_node2 = Node(['1', '2', '0', '4', '5', '3', '7', '8', '6'], None, None)
@@ -10,5 +11,7 @@ goal_node = Node(['1', '2', '3', '4', '5', '6', '7', '8', '0'], None, None)
 # agent.solve()
 # print(agent.h_manhattan(test_node))
 # print(test_node1 == start_node)
-agent = Expand_UCS(start_node, goal_node)
+# agent = Expand_UCS(start_node, goal_node)
+# agent.solve()
+agent = Expand_BFS(start_node, goal_node)
 agent.solve()
